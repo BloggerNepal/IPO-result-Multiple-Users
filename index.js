009e07140b26b5ede851321132600cb268708516
Sagar Devkota <info@devkotasagar.com.np>
2022-03-01 13:17:13 +0545

    initial commit

---
+++ index.js
@@ -0,0 +1,105 @@
+const axios = require('axios');
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const requestHandeler = (req, res) => {
+    let method = req.method;
+    let url = req.url;
+
+    let queries = '';
+
+    let urlSplit = url.split("?");
+
+    url = urlSplit[0]
+    if (urlSplit.length > 1) {
+        queries = urlSplit[1]
+    }
+
+    console.log(method, url, queries);
+
+    if (method == "GET" && (url == "/" || url == "/index.css" || url == "/index.js")) {
+        let file;
+        let contentType;
+        if (url == "/" || url == 'index.html') {
+            file = 'public/index.html';
+            contentType = 'text/html'
+        }
+        if (url == '/index.css') {
+            file = 'public/index.css';
+            contentType = 'text/css'
+        }
+        if (url == '/index.js') {
+            file = 'public/index.js';
+            contentType = 'application/javascript'
+        }
+        var filePath = path.join(__dirname, file);
+        var stat = fs.statSync(filePath);
+
+        res.writeHead(200, {
+            'Content-Type': contentType,
+            'Content-Length': stat.size
+        });
+
+        var readStream = fs.createReadStream(filePath);
+        readStream.pipe(res);
+        return;
+    }
+
+    if (method == "POST" && url == "/") {
+
+        let body = '';
+
+        req.on('data', (chunk) => {
+            body += chunk;
+        });
+
+        req.on('end', () => {
+            let data = JSON.parse(body);
+            // console.log(data);
+
+            let promises = [];
+            data.boidArray.forEach(element => {
+                promises.push(getIPOResult(element, data.companyShareId));
+            });
+
+            Promise.all(promises).then(results => {
+                // console.log(results);
+
+                res.writeHead(200, {
+                    'Content-Type': "application/json",
+                });
+
+                let response = {
+                    results: results,
+                    boidArray: data.boidArray
+                }
+
+                res.write(JSON.stringify(response));
+                res.end();
+
+            })
+
+        });
+        return;
+    }
+
+    res.writeHead(400, {
+        "Content-Type": "text/plain"
+    });
+    res.end("Not Found");
+}
+
+const server = http.createServer(requestHandeler);
+server.listen(5001);
+console.log("Server Started at 5001");
+
+const getIPOResult = (boid, company) => {
+    return axios.post("https://iporesult.cdsc.com.np/result/result/check", {
+        boid: boid,
+        companyShareId: company
+    }).then(response => {
+        return response.data
+    })
+}
---
+++ public/index.js
@@ -0,0 +1,110 @@
+let companyShareElement;
+
+const getCompany = () => {
+    axios.get("https://iporesult.cdsc.com.np/result/companyShares/fileUploaded").then(response => {
+        // console.log(response);
+        return response.data.body
+    }).then(data => {
+        // console.log(data);
+        let conpanySelectElement = document.getElementById("companySelect");
+        data.forEach(element => {
+            let aOption = document.createElement('option');
+            aOption.innerText = element.name
+            aOption.setAttribute("value", element.id)
+            conpanySelectElement.appendChild(aOption);
+        });
+    })
+}
+
+getCompany();
+
+const viewResult = () => {
+    clearResultAndError();
+    console.log("viewResult");
+    let boids = document.getElementById('boids').value;
+
+    let company = document.getElementById("companySelect").value;
+
+    let boidArray = [];
+    let boidsA = boids.split(",")
+    boidsA.forEach(element => {
+        let id = element.trim();
+
+        if (id.length == 16) {
+            if (!isNaN(id)) {
+                boidArray.push(id);
+
+            } else {
+                showError(id);
+            }
+        } else {
+            showError(id);
+        }
+    });
+
+    if (boidArray.length > 0) {
+        axios.post("/", {
+            boidArray,
+            companyShareId: company
+        }).then(response => {
+            return response.data
+        }).then(data => {
+            // console.log(data);
+            data.results.forEach((element, index) => {
+                if (!element.success) {
+                    showNotAlloted(data.boidArray[index])
+                }
+                if (element.success) {
+                    let allotedAmount = element.message.split(":")[1].trim();
+                    showAlloted(data.boidArray[index], allotedAmount);
+                }
+            });
+        }).catch(error => {
+            console.log("error", error);
+        })
+    }
+
+    // if we can bypass cors on browser no need of backend
+    // boidArray.forEach(element => {
+    //     console.log(element);
+    //     axios.post("https://iporesult.cdsc.com.np/result/result/check", {
+    //         boid: element,
+    //         companyShareId: company
+    //     }).then(response => {
+    //         return response.data
+    //     }).then(data => {
+    //         console.log(data);
+    //     }).catch(error => {
+    //         console.log("error", error);
+    //     })
+    // });
+
+}
+
+const showError = (id) => {
+    let message = `${id} is not a valid BOID.`
+    let span = document.createElement('span');
+    span.innerText = message;
+    document.getElementById("error").appendChild(span);
+}
+
+const showAlloted = (id, quantity) => {
+    message = `${id} - Alloted | Quantity: ${quantity}`;
+    let span = document.createElement('span');
+    span.setAttribute("class", "alloted")
+    span.innerText = message;
+    document.getElementById("result").appendChild(span);
+}
+
+const showNotAlloted = (id) => {
+    message = `${id} - Not Alloted.`
+    let span = document.createElement('span');
+    span.setAttribute("class", "not-alloted")
+    span.innerText = message;
+    document.getElementById("result").appendChild(span);
+}
+
+const clearResultAndError = () => {
+    document.getElementById("error").innerText = ""
+    document.getElementById("result").innerText = ""
+}
